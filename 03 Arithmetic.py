2cf7e3e8a4c9d3f81964352dd4ecf091babcedd7
patlori vishnuvardhan <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2025-01-28 22:46:14 +0530

    Arithmetic.py

---
+++ 03 Arithmetic.py
@@ -0,0 +1,22 @@
+# In this code we discuss about arithmetic operations 
+
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+
+# here "int" stands for input type should be integer
+# "input()" stands for reading input from the user
+
+# Addition of two numbers
+add=num1+num2
+print(add)
+# Substraction of two numbers 
+sub=num1-num2
+print(sub)
+# Multiplication of two numbers
+mult=num1*num2
+print(mult)
+# Division of two numbers
+div=num1//num2
+# here we use // to get integer value 
+# if we use / we get floating value
+print(div)
