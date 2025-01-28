56f48f1c2830fce70391785c7d210850bf2713c7
patlori vishnuvardhan <129029564+vishnuvardhanpatlori@users.noreply.github.com>
2025-01-28 22:18:18 +0530

    02 Variable.py

---
+++ 02 Variable.py
@@ -0,0 +1,11 @@
+# in this we discuss about varialbes
+# in python we can declare variable directly by name of the variable
+# Syntax Var_Name = value
+
+Name ="Vishnuvardhan" #string
+Age = 20 #integer
+College = "CMR"
+Grade = 'A' #Character
+
+# now printing\Accessing  the above variables by variable names 
+print("My name is ",Name, "and my age is ",Age, ". I;m curenntly studing in ",College)
